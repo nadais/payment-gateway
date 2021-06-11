417fcb0571fe11a7cab8bef6ba31bca5ef55314e
Joao Nadais <2493014+nadais@users.noreply.github.com>
2021-06-11 16:53:51 +0200

    Remove dependency on appDbContext from startup

--- src/PaymentGateway.Api/Startup.cs
+++ src/PaymentGateway.Api/Startup.cs
@@ -39,8 +39,7 @@ namespace PaymentGateway.Api
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app,
-            IWebHostEnvironment env,
-            AppDbContext dbContext)
+            IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
